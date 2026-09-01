0385056513b5596de13ee2ce36caf44b4d236f44
Aisyah Nabila Nur Affifah <aisyahnabilana@gmail.com>
2026-09-01 10:15:27 +0700

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// PENTING: ganti '/REPO_NAME/' sesuai nama repository GitHub kamu.
-// Jika deploy ke <username>.github.io (root domain), gunakan base: '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/REPO_NAME/',
+  base: '/web-koleksi-ruang-langka/',
 })
